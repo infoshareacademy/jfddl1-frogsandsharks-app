adeef1dda8ffb52b1cbed2cf113a6419804b7240
Przemyslaw <p.posiej@gmail.com>
2017-05-12 16:46:55 +0200

    plan.js

---
+++ src/Plan.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from 'react-bootstrap'
+
+class Plan extends React.Component {
+render() {
+    return (
+    <h1>
+        Twój plan żywieniowy
+    </h1>
+    <Button bsStyle="primary">Primary</Button>
+)}}
+export default Plan
