f9e6538caf5431acb55d05619b6db2f1d1e84776
Andrzej <andrzej.szadkowski89@gmail.pl>
2017-05-16 16:40:50 +0200

    add table with data

--- src/ListOfEffects.js
+++ src/ListOfEffects.js
@@ -9,7 +9,7 @@ import {
   NavItem,
   Panel,
   ListGroup,
-  Accordion
+  ListGroupItem
 } from 'react-bootstrap'
 
 
@@ -19,49 +19,29 @@ class ListOfEffects extends React.Component {
     return (
       <Grid>
         <Row className="show-grid">
-          <Col xs={12} md={8} lg={6}>
-            <div>blablabla</div>
+          <Col xs={12} md={4} lg={4}>
+            <Row>
+              <Col lg={12}>
+                <div>Photo</div>
+              </Col>
+              <Col lg={12}>
+                <div>Button</div>
+              </Col>
+            </Row>
           </Col>
-          <Col xs={6} md={4} lg={3}>
-            <Panel collapsible defaultExpanded header="Dieta">
-              <ListGroup fill>
-                <Accordion>
-                  <Panel header="Owoce i Warzywa" eventKey="1">
-                    Anim pariatu
-                  </Panel>
-                  <Panel header="Mięso, ryby, garmaż" eventKey="2">
-                    Anim pariatur
-                  </Panel>
-                  <Panel header="Nabiał i jaja" eventKey="4">
-                    Anim pariatur
-                  </Panel>
-                  <Panel header="Pieczywo" eventKey="5">
-                    Anim pariatur
-                  </Panel>
-                  <Panel header="Produkty sypkie i makarony" eventKey="6">
-                    Anim pariatur
-                  </Panel>
-                  <Panel header="Sosy, oleje, ocet" eventKey="7">
-                    Anim pariatur
-                  </Panel>
-                  <Panel header="Przetwory" eventKey="8">
-                    Anim pariatur
-                  </Panel>
-                  <Panel header="Słodycze" eventKey="9">
-                    Anim pariatur
-                  </Panel>
-                  <Panel header="Przyprawy" eventKey="10">
-                    Anim pariatur
-                  </Panel>
-                  <Panel header="Zdrowa Żywność" eventKey="11">
-                    Anim pariatur
-                  </Panel>
-                  <Panel header="Kawa, herbata, napoje" eventKey="12">
-                    Anim pariatur
-                  </Panel>
-                </Accordion>
+          <Col xs={12} md={5} lg={5}>
+
+              <ListGroup>
+                <h1>W 100 gramach produktu</h1>
+                <ListGroupItem>kcal : </ListGroupItem>
+                <ListGroupItem>Białko : </ListGroupItem>
+                <ListGroupItem>Węglowodany : </ListGroupItem>
+                <ListGroupItem>Tłuszcze : </ListGroupItem>
               </ListGroup>
-            </Panel>
+
+          </Col>
+          <Col xs={12} md={3} lg={3}>
+            <div>Button</div>
           </Col>
         </Row>
       </Grid>
@@ -69,7 +49,6 @@ class ListOfEffects extends React.Component {
 
   }
 }
-
 
 
 export default ListOfEffects
