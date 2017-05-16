e84d8cc8d6ad90d93e255cb4feeb6d89c7916811
Damian <dbiszczak@gmail.com>
2017-05-16 16:49:42 +0200

    adding routing to menu and layout in dashboard

--- src/App.js
+++ src/App.js
@@ -1,21 +1,83 @@
-import React, { Component } from 'react';
+import React from 'react'
 import {
-    BrowserRouter as Router,
-    Route, Link
+  BrowserRouter as Router,
+  Route
 } from 'react-router-dom'
+import {
+  Grid,
+  Col,
+  Row
+} from 'react-bootstrap'
 
+import Home from './Home'
+import FirstPage from './FirstPage'
 
-import Navigation from './Navigation'
-import MySideNav from './MySideNav'
-import './App.css';
+import BurgerMenuWrapper from './BurgerMenuWrapper'
 
+const links = [
+  { path: '/', label: 'Home' },
+  { path: '/firstpage', label: 'FirstPage' },
+]
 
-class App extends Component {
-  render() {
-   return (
-      <Router><MySideNav/></Router>
-   )
+class App extends React.Component {
+
+  state = {
+    sidebarOpen: false
   }
+
+  toggleSidebar = (shouldBecomeOpen) => this.setState({
+    sidebarOpen: shouldBecomeOpen
+  })
+
+  render = () => (
+    <Router>
+      <BurgerMenuWrapper
+        isOpen={this.state.sidebarOpen}
+        toggleSidebar={this.toggleSidebar}
+        onStateChange={(state) => this.toggleSidebar(state.isOpen)}
+        links={links}
+      >
+        <Grid>
+
+
+          <Row>
+            <Col md={12}>
+              <Route exact path="/" component={Home}/>
+              <Route path="/firstpage" component={FirstPage}/>
+            </Col>
+          </Row>
+        </Grid>
+      </BurgerMenuWrapper>
+    </Router>
+  )
 }
 
 export default App;
+
+///////////////////////////////////////////////////////////////////////////
+
+
+/*
+import Topics from './Topics'
+import Students from './Students'
+import Student from './Student'
+import Groups from './Groups'
+import Group from './Group'
+import Counters from './Counters'
+import Counter from './Counter'
+
+import BurgerMenuWrapper from './BurgerMenuWrapper'
+import TopNavigation from './TopNavigation'
+
+const links = [
+  { path: '/', label: 'Home' },
+  { path: '/about', label: 'About' },
+  { path: '/topics', label: 'Topics' },
+  { path: '/students', label: 'Students' },
+  { path: '/groups', label: 'Groups' },
+  { path: '/counters', label: 'Counters' },
+  { path: '/counter', label: 'Counter' }
+]
+
+
+export default App*/
--- src/Home.js
+++ src/Home.js
@@ -15,10 +15,12 @@ import {
     LinkContainer
 } from 'react-router-bootstrap'
 
+import './home.css'
+
 const Home = () => (
     <Grid>
-        <Row className="show-grid">
-            <Col xs={12} md={12} lg={12}>Logo pTrainer</Col>
+        <Row className="show-grid logoSpace">
+            <Col xs={12} md={12} lg={12}><img src="https://s-media-cache-ak0.pinimg.com/736x/33/b8/69/33b869f90619e81763dbf1fccc896d8d.jpg" id="logo"/></Col>
         </Row>
         <Row className="show-grid">
             <Col xs={12} md={12} lg={12}>
@@ -27,9 +29,9 @@ const Home = () => (
                  jsdkfskdjf sdjkf jsdlfjsdklfjs dfkl jfslkdjf sldfjsldj fsdklfj kjf sdjsd flksdjf</p>
             </Col>
         </Row>
-        <Row className="show-grid">
+        <Row className="show-grid options">
             <Col xs={6} md={4}>
-                <Thumbnail src="/assets/thumbnaildiv.png" alt="242x200">
+                <Thumbnail src="http://www.iconsfind.com/wp-content/uploads/2015/10/20151012_561baa03bcbc6.png" alt="250x250" >
                     <h3>Thumbnail label</h3>
                     <p>Description</p>
                     <p>
