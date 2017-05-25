84211d1bca72b8ab9169a85f84a434eee65ed2a5
Przemyslaw <p.posiej@gmail.com>
2017-05-25 16:08:04 +0200

    add tooltips to fooood plan

--- src/FoodPlan/Meal.js
+++ src/FoodPlan/Meal.js
@@ -1,21 +1,28 @@
 import React from 'react'
 import {LinkContainer} from 'react-router-bootstrap'
 import './style.css'
+import {Tooltip, OverlayTrigger} from 'react-bootstrap'
+
+const tooltip = (
+  <Tooltip id="tooltip"><strong>Kliknij tutaj</strong> aby dodać produkt.</Tooltip>
+);
 
 const Meal = (props) => (
 
   <LinkContainer to="foods">
     <div>
       <p className="mealNameStyle">{props.mealName}</p>
-      <div className="mealStyle">
-        <ul>
-          {
-            props.products.map(
-              (product, index) => <li key={index}>{product.name}</li>
-            )
-          }
-        </ul>
-      </div>
+      <OverlayTrigger placement="top" overlay={tooltip}>
+        <div className="mealStyle" bsStyle="default">
+          <ul >
+            {
+              props.products.map(
+                (product, index) => <li key={index}>{product.name}</li>
+              )
+            }
+          </ul>
+        </div>
+      </OverlayTrigger>
     </div>
   </LinkContainer>
 )
