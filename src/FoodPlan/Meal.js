c03743ab7ee6c33b48f32b686c4cea0153ab67c9
Przemyslaw <p.posiej@gmail.com>
2017-05-31 16:46:46 +0200

    remove from list - failed

--- src/FoodPlan/Meal.js
+++ src/FoodPlan/Meal.js
@@ -3,33 +3,43 @@ import {LinkContainer} from 'react-router-bootstrap'
 import './style.css'
 import {Tooltip, OverlayTrigger} from 'react-bootstrap'
 
+import { connect } from 'react-redux'
+
+import { remove } from "../state/selections"
+
 const tooltip = (
   <Tooltip id="tooltip"><strong>Kliknij tutaj</strong> aby dodać produkt.</Tooltip>
 );
 
-const Meal = (props) => (
 
-  <div>
-    <div>
-      <p className="mealNameStyle">{props.mealName}</p>
-    </div>
-    <div className="mealStyle">
+export default connect (
+  dispatch => ({
+    removeFromFavorites: (productId) => dispatch(remove(productId))
+  })
+)(
+  function Meal(props) {
+    return (
       <div>
-        <OverlayTrigger placement="top" overlay={tooltip}>
-          <LinkContainer to={props.link} className="imgStyle">
-            <img src={require('./ico/add.png')} alt=""/>
-          </LinkContainer>
-        </OverlayTrigger>
-        <ul>
-          {
-            props.products.map(
-              (product, index) => <li key={index}>{product.name}</li>
-            )
-          }
-        </ul>
+        <div>
+          <p className="mealNameStyle">{props.mealName}</p>
+        </div>
+        <div className="mealStyle">
+          <div>
+            <OverlayTrigger placement="top" overlay={tooltip}>
+              <LinkContainer to={props.link} className="imgStyle">
+                <img src={require('./ico/add.png')} alt=""/>
+              </LinkContainer>
+            </OverlayTrigger>
+            <ul>
+              {
+                props.products.map(
+                  (product, index) => <li key={index} onClick={() => props.removeFromFavorites(product)}>{product.name}</li>
+                )
+              }
+            </ul>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
+    )
+  }
 )
-
-export default Meal
--- src/state/selections.js
+++ src/state/selections.js
@@ -1,9 +1,16 @@
 const ADD = 'selections/ADD'
+const REMOVE = 'selections/REMOVE'
 
 export const add = (day, meal, productId) => ({
   type: ADD,
   day, meal, productId
 })
+
+export const remove = (day, meal, productId) => ({
+  type: REMOVE,
+  day, meal, productId
+})
+
 
 const initialState = []
 
@@ -17,6 +24,10 @@ export default (state = initialState, action) => {
           meal: action.meal,
           productId: action.productId}
       ]
+    case REMOVE:
+      return state.filter(
+        productId => productId !== action.productId
+      )
     default:
       return state
   }
